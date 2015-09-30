c21fcb54600d560968b3e5393e7b9afce61e633c
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-10-01 00:44:19 +0700

    Add menu

--- AnthonyGanteng.java
+++ AnthonyGanteng.java
@@ -2,7 +2,131 @@ import java.util.*;
 import java.io.*;
 
 public class AnthonyGanteng {
+
+	InputReader mainProgramReader, inputReader;
+	BufferedWriter outputWriter;
+
+	String inputString;
+	int inputInt;
+
+	void run() throws Exception {
+		init();
+		launchSettings();
+		launchMenu();
+	}
+
+	void init() {
+		mainProgramReader = new InputReader(new BufferedReader(new InputStreamReader(System.in)));
+	}
+
+	void launchSettings() throws IOException {
+		System.out.println("/-Settings-/");
+		System.out.print("Masukan berasal dari mana? (stdin/file): ");
+		inputString = mainProgramReader.nextString();
+		if (inputString.charAt(0) == 's') {
+			inputReader = new InputReader(new BufferedReader(new InputStreamReader(System.in)));
+		}
+		if (inputString.charAt(0) == 'f') {
+			System.out.print("Masukan nama file input: ");
+			inputString = mainProgramReader.nextString();
+			try {
+				inputReader = new InputReader(new BufferedReader(new InputStreamReader(new FileInputStream(inputString))));				
+			} catch (FileNotFoundException e) {
+				System.out.println("File tidak ditemukan");
+			}
+
+		}
+		System.out.print("Keluaran ditulis kemana? (stdout/file): ");
+		inputString = mainProgramReader.nextString();
+		if (inputString.charAt(0) == 's') {
+			outputWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		}
+		if (inputString.charAt(0) == 'f') {
+			System.out.print("Masukan nama file input: ");
+			inputString = mainProgramReader.nextString();
+			try {
+				outputWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(inputString)));
+			} catch (FileNotFoundException e) {
+				System.out.println("File tidak ditemukan");
+			}
+
+		}
+	}
+
+	void launchMenu() throws IOException {
+		System.out.println();
+		System.out.println("===================");
+		System.out.println("TUGAS BESAR ALGEO 1");
+		System.out.println("===================");
+		System.out.println("Menu:");
+		System.out.println("1. Menyelesaikan Matriks");
+		System.out.println("2. Menyelesaikan SPL");
+		System.out.println("3. Menyelesaikan Interpolasi");
+		System.out.println("4. Settings");
+		System.out.println("5. About");
+		System.out.println("6. Exit");
+		System.out.print("Pilihan Anda: ");
+		inputInt = mainProgramReader.nextInt();
+		switch (inputInt) {
+			case 1: {
+				launchMatriks();
+				break;
+			}
+			case 2: {
+				launchSPL();
+				break;
+			}
+			case 3: {
+				launchInterpolasi();
+				break;
+			}
+			case 4: {
+				launchSettings();
+				break;
+			}
+			case 5: {
+				launchAbout();
+				break;
+			}
+			case 6: {
+				System.exit(0);
+				break;
+			}
+			default: {
+				System.out.println("Pilihan Salah!");
+			}
+		}
+		System.out.println("Press Enter to continue.");
+		inputReader.nextChar();
+		launchMenu();
+	}
+
+	void launchMatriks() {
+
+	}
+
+	void launchSPL() {
+
+	}
+
+	void launchInterpolasi() {
+
+	}
+
+	void launchAbout() {
+		System.out.println("/-About-/");
+		System.out.println("Dibuat oleh:");
+		System.out.println("13514003 - Davin Prasetya");
+		System.out.println("13514083 - Nathan J. Runtuwene");
+		System.out.println("13514085 - Christian A. Setyawan");
+	}
+
 	public static void main(String[] args) {
-		System.out.println("ANTHONY GANTENG BANGETS");
+		try {
+			new AnthonyGanteng().run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
